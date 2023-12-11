53468d67ac29799bf48f1ad0bb7caecacb795588
MaksGorobets <gorobets542@gmail.com>
2023-12-11 16:31:43 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Maks Winters on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
